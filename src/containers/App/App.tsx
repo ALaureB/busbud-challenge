9535f12a053e4e7ee89a76dcbfdee5227c06516a
Anne-Laure Bienaimé <annelaure.bienaime@gmail.com>
2020-10-02 21:21:12 -0400

    Init front-end busbud challenge

---
+++ src/containers/App/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.scss";
+
+import Container from "react-bootstrap/Container";
+
+const App: React.FC = () => {
+  return (
+    <Container className="busbud-app">
+      <h1>Busbud challenge</h1>
+    </Container>
+  );
+};
+
+export default App;

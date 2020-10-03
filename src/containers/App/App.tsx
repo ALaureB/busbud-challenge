fea8ac89b37759740409d679557d28212d68a8fe
Anne-Laure Bienaimé <annelaure.bienaime@gmail.com>
2020-10-02 23:28:11 -0400

    Update typescript to support react-bootstrap types

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import "./App.scss";
 
 import Container from "react-bootstrap/Container";
+
 import Header from "../Header/Header";
+import Menu from "../../components/Menu/Menu"
 
 const App: React.FC = () => {
   return (
     <Container fluid className="bosheaga-app p-0">
+      <Menu />
       <Header />
     </Container>
   );
